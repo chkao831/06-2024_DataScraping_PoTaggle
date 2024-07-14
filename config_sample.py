f2f57bda7c4ce4c58529cee3f9351392615a1163
Carolyn Kao <chkao831@CarolyntekiMacBook-Pro.local>
2024-07-15 00:55:14 +0100

    modified sample config

--- config_sample.py
+++ config_sample.py
@@ -1,12 +1,15 @@
-## A sample file for config.py. The file used in the demo is checked into the repo for security concern.
+## A sample file for config.py. 
+# The real file used in the demo is not checked into the repo for security concern.
+# Please add content for config.py according to the samples below. 
 
 # to get one: https://ai.google.dev/gemini-api/docs/api-key
 API_KEY = 'your_api_key' 
 
-# the urls for data scraping
+# the news urls for data scraping
 url_list = ['https://url_1.com', 'https://url_2.com'] 
 
 # the question prompt for the model
 question_prompt = """
-I have some text from the news, could you tell me...
+I have some text from the news, could you tell me the organization name,
+ticker, news release information, call information......
 """
